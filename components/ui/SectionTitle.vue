0e3f667bdd7a69f464a0cced9631ec72f40e4539
pedrodruviaro <pedrodruviaro@gmail.com>
2024-02-18 10:55:19 -0300

    add: SectionTitle component

---
+++ components/ui/SectionTitle.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+interface Props {
+  text: string
+  as?: string
+}
+
+withDefaults(defineProps<Props>(), {
+  as: "h2",
+})
+</script>
+
+<template>
+  <Component
+    :is="as"
+    class="font-black uppercase text-4xl md:text-5xl lg:text-6xl"
+  >
+    {{ text }}
+  </Component>
+</template>
