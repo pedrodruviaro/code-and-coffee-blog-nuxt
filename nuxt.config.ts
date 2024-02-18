fa2a87992c6958f59c1bfaf19e3ede435b0e7122
pedrodruviaro <pedrodruviaro@gmail.com>
2024-02-18 10:13:35 -0300

    add: nuxt apollo

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/google-fonts"],
+  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/google-fonts", "@nuxtjs/apollo"],
 
   googleFonts: {
     families: {
       "Work Sans": [400, 600, 700, 900],
+    },
+  },
+
+  apollo: {
+    clients: {
+      default: {
+        httpEndpoint:
+          "https://api-sa-east-1.hygraph.com/v2/clsq0nu2o0pwv01umyr941v6j/master",
+      },
     },
   },
 })
