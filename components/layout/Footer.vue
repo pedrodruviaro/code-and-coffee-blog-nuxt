68854ee5061e4e418d7c8915c32d3e1d52b5ead0
pedrodruviaro <pedrodruviaro@gmail.com>
2024-02-18 15:23:27 -0300

    add: new footer styles and news form

--- components/layout/Footer.vue
+++ components/layout/Footer.vue
@@ -2,19 +2,40 @@
 const currentYear = computed(() => {
   return new Date().getFullYear()
 })
+
+const enteredEmail = ref("")
+
+function handleSubmit() {
+  console.log("Good that you are testing! Got ya!")
+  enteredEmail.value = ""
+}
 </script>
 
 <template>
-  <footer
-    class="bg-brand-black text-brand-white py-8 text-center relative place-items-end md:place-items-center grid overflow-hidden min-h-60 lg:min-h-96"
-  >
-    <UiContainer class="z-[2]">
-      <p class="text-sm bg-brand-black max-w-max mx-auto p-3">
-        &copy; Copyright Code & Coffee - {{ currentYear }}
-      </p>
+  <footer class="bg-brand-black text-brand-white relative overflow-hidden">
+    <UiContainer as="section" class="py-10 md:py-14 lg:py-20">
+      <div class="grid gap-8 md:grid-cols-[max-content_1fr] items-center">
+        <UiSectionTitle text="Code news to your inbox" class="max-w-[15ch]" />
+
+        <form class="flex gap-2 flex-wrap" @submit.prevent="handleSubmit">
+          <input
+            required
+            v-model="enteredEmail"
+            type="email"
+            placeholder="your.email@provider.com"
+            class="text-brand-black p-2 focus:outline-brand-main w-full max-w-[22ch]"
+          />
+          <button
+            type="submit"
+            class="text-brand-black bg-brand-white px-4 py-2 hover:bg-opacity-80 focus:outline-brand-main"
+          >
+            Sign up
+          </button>
+        </form>
+      </div>
     </UiContainer>
 
-    <div class="z-[1] absolute inset-0">
+    <div class="w-full">
       <img
         src="~/assets/images/footer-bg.png"
         alt=""
@@ -26,5 +47,11 @@ const currentYear = computed(() => {
         height="750"
       />
     </div>
+
+    <UiContainer as="section" class="z-[2]">
+      <p class="text-sm bg-brand-black max-w-max mx-auto py-6">
+        &copy; Copyright Code & Coffee - {{ currentYear }}
+      </p>
+    </UiContainer>
   </footer>
 </template>
