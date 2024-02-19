0163cdbff48983db5185dfad18c20f61ffa4855b
pedrodruviaro <pedrodruviaro@gmail.com>
2024-02-19 11:30:49 -0300

    add: Ui Badge component

---
+++ components/ui/Badge.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+interface Props {
+  text: string
+}
+
+defineProps<Props>()
+</script>
+
+<template>
+  <span
+    class="border-2 border-brand-black rounded-full px-3 py-1 font-semibold uppercase block max-w-max"
+    >{{ text }}</span
+  >
+</template>
