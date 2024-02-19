e9e87cf10ab96fbe5ad86685f43e2397a2cff55f
pedrodruviaro <pedrodruviaro@gmail.com>
2024-02-19 18:16:07 -0300

    add: link to /posts

--- pages/index.vue
+++ pages/index.vue
@@ -23,7 +23,13 @@ useSeoMeta({
 
     <PagesHomeRecentPosts />
 
-    <section class="mt-10 lg:mt-14">
+    <NuxtLink
+      to="/posts"
+      class="font-black uppercase text-lg md:text-3xl mt-5 block"
+      >See all posts</NuxtLink
+    >
+
+    <section class="mt-10 lg:mt-14 border-t-4 pt-10 lg:pt-14 border-brand-gray">
       <UiSectionTitle text="Authors" />
       <AuthorsListAll class="mt-8" />
     </section>
