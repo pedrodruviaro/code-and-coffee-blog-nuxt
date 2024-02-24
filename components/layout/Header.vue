867b7d50fc8cf68e4462b1c60920a64f94523c3b
pedrodruviaro <pedrodruviaro@gmail.com>
2024-02-24 18:19:00 -0300

    hotfix: rename header link

--- components/layout/Header.vue
+++ components/layout/Header.vue
@@ -3,7 +3,7 @@
     <NuxtLink
       to="/"
       class="font-bold uppercase block max-w-max relative after:w-full after:bg-transparent after:transition-all after:h-1 after:block after:bottom-0 after:absolute hover:after:bg-brand-main"
-      >CODE & COFFEE BLOG</NuxtLink
+      >CODE & COFFEE</NuxtLink
     >
   </UiContainer>
 </template>
